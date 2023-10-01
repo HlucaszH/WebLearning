c5be948892c771bb4e6fe2e17aa17d4605e26c32
HlukaszH <thelucasholecek@gmail.com>
2023-10-01 15:49:06 +0200

    js practice

---
+++ pjctJS/index.js
@@ -0,0 +1,27 @@
+console.log("Hello, World!");
+console.log("Hmm");
+let $ = 1; // declared a variable with the name "$"
+let _ = 2; // and now a variable with the name "_"
+//alert($ + _);
+const bday = "08.10";
+const name = "Eclipse";
+const age = "19";
+console.log(bday);
+console.log(name);
+console.log(age);
+let x = 8;
+let y = 2;
+x *= y + 2;
+let z = x * y;
+console.log(z);
+document.getElementById("multi").innerHTML = z;
+let s = "my" + " pp" + " small";
+let b = 1;
+//alert(b++);
+//alert(b);
+console.log(b);
+const newline = "One day you finally knew\nwhat you had to do, and began,";
+console.log(newline);
+let text = "Apple, Banana, Kiwi";
+let part = text.slice(7)
+document.getElementById("slices").innerHTML = part;
